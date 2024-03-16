df6d26c8ae59d1a7da6f7a6a3b589fa47d15c102
Grant Ramsay <seapagan@gmail.com>
2024-03-16 14:29:01 +0000

    hilight the log-level string if it exists
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- fastapi_tui/log_viewer.py
+++ fastapi_tui/log_viewer.py
@@ -2,7 +2,22 @@
 
 from __future__ import annotations
 
+import re
+from enum import Enum
+
 from textual.widgets import RichLog
+
+
+class LogLevel(Enum):
+    """Enum for log levels.
+
+    These are from memory and need checking!
+    """
+
+    DEBUG = "gray"
+    INFO = "green"
+    WARNING = "yellow"
+    ERROR = "red"
 
 
 class LogViewer(RichLog):
@@ -22,4 +37,22 @@ class LogViewer(RichLog):
 
     def add(self, line: str) -> None:
         """Add a line to the log."""
-        self.write(line)
+        log_level, message = self.split_text(line)
+        if log_level:
+            log_color = LogLevel[log_level.strip()[:-1].upper()].value
+            self.write(f"[{log_color}]{log_level}[/{log_color}]{message}")
+        else:
+            self.write(f"{line}")
+
+    def split_text(self, input_text: str) -> tuple[str | None, str]:
+        """Split out the LOG_LEVEL from the message if present.
+
+        Return a tuple of (LOG_LEVEL, message) or (None, message) if no
+        LOG_LEVEL string is found.
+        """
+        pattern = r"^([^:]*:\s+)(.*)|^(.*)$"
+        match = re.search(pattern, input_text)
+
+        if match and match.group(1):
+            return (match.group(1), match.group(2))
+        return None, input_text
